bc83bdf0a362adc79e7e8b2007d005760c0a5b9c
Kasamba5i <141848316+Kasamba5i@users.noreply.github.com>
2024-08-20 11:12:29 +0100

    update 1 du 20_08_2024

---
+++ bootstrap-5.0.2-dist/assets/js/mon-script.js
@@ -0,0 +1,16 @@
+function isValidPhoneNumber(phoneNumber) {
+    // Expression régulière pour vérifier la validité d'un numéro de téléphone
+    // On vérifie si le numéro contient uniquement des chiffres et s'il a une longueur de 10 ou 12 caractères
+    var regex = /^\d{10}$|^\d{12}$/;
+    return regex.test(phoneNumber);
+}
+
+function isValidLicensePlate(licensePlate) {
+    // Expression régulière pour vérifier la validité d'une plaque d'immatriculation
+    /*
+    var regex = /^[A-Za-z0-9]{6}\s[A-Za-z0-9]{2}$/;
+    return regex.test(licensePlate);
+    */
+    var regex = /^[0-9]{4}[A-Za-z]{2}[0-9]{2}$/;
+    return regex.test(licensePlate);
+}
